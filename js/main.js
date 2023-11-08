4d4835c8220c524b4c7bcb90b4aa4969cb1a6903
ShodiyorShukurov <shodiyorshukurov07@gmail.com>
2023-11-08 11:02:59 +0500

    NFT

---
+++ js/main.js
@@ -0,0 +1,34 @@
+const burgerMenu = document.querySelector(".site-header__mobile");
+const mobileHeader = document.querySelector(".site-header__list-mobile");
+const accordionBtn = document.querySelector(".site-header__accordion-btn");
+const line = document.querySelector("#line");
+
+let menu = false;
+
+burgerMenu.addEventListener("click", function () {
+  if (!menu) {
+    burgerMenu.src = "../images/icons/close.svg";
+    mobileHeader.style.display = "flex";
+    menu = true;
+  } else {
+    burgerMenu.src = "../images/icons/burger.svg";
+    mobileHeader.style.display = "none";
+    menu = false;
+  }
+  //   burgerMenu.src = '../images/icons'
+});
+
+let linebtn = false;
+accordionBtn.addEventListener("click", function () {
+  if (!linebtn) {
+    console.log("create");
+    line.style.rotate = "180deg";
+    linebtn = true;
+  } else {
+    line.style.rotate = "0deg";
+    linebtn = false;
+  }
+  //   burgerMenu.src = '../images/icons'
+});
+
+
